71a1ce13755725dabe43dd8c624450720f91c7b7
gcosano <germancosanof@gmail.com>
2021-03-11 14:43:49 +0100

    creation

---
+++ projects/security/src/lib/security.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { SecurityComponent } from './security.component';
+
+
+
+@NgModule({
+  declarations: [SecurityComponent],
+  imports: [
+  ],
+  exports: [SecurityComponent]
+})
+export class SecurityModule { }
---
+++ projects/security/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of security
+ */
+
+export * from './lib/security.service';
+export * from './lib/security.component';
+export * from './lib/security.module';
